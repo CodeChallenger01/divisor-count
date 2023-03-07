c0b4de3bbdea3ade6b7d495ce346e804feddf495
Manish Mishra <manish.mishra@knoldus.com>
2023-03-07 21:46:02 +0530

    Updated Main File

--- count-divisor/src/main/scala/Main.scala
+++ count-divisor/src/main/scala/Main.scala
@@ -1,5 +1,34 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
+import scala.io.StdIn.readInt
+
+class Count{
+  def countDivisor(firstNum: Int, secondNum: Int, thirdNum: Int): Int = {
+    var count:Int=0
+    for(index<-firstNum to secondNum){
+      if(index%thirdNum==0){
+        count+=1
+      }
+    }
+    return count
+  }
+}
+
+object Main extends App{
+  private val obj = new Count
+  private val firstNum: Int = readInt()
+  private val secondNum: Int = readInt()
+  private val thirdNum: Int = readInt()
+  try{
+    if(thirdNum == 0){
+      throw new ArithmeticException
+    }
+    else{
+      val count: Int = obj.countDivisor(firstNum, secondNum, thirdNum)
+      print("Total Count :" + count)
+    }
+  }
+  catch{
+    case ex: ArithmeticException =>{
+      print(" Divide by Zero Exception [Divisor Cannot be Zero]")
+    }
   }
 }
