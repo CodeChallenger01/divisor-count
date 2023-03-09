e99b15e81adc9108f18e92815560fd2f95c30865
Manish Mishra <manish.mishra@knoldus.com>
2023-03-09 12:08:41 +0530

    updated

--- count-divisor/src/main/scala/Main.scala
+++ count-divisor/src/main/scala/Main.scala
@@ -1,22 +1,23 @@
 import scala.io.StdIn.readInt
-
 class Count{
+  //countDivisor Method to count the value and return
   def countDivisor(firstNum: Int, secondNum: Int, thirdNum: Int): Int = {
-    var count:Int=0
-    for(index<-firstNum to secondNum){
-      if(index%thirdNum==0){
+    var count: Int = 0
+    for(index <- firstNum to secondNum){
+      if(index % thirdNum == 0){
         count+=1
       }
     }
     return count
   }
 }
-
+//singleton object
 object Main extends App{
   private val obj = new Count
   private val firstNum: Int = readInt()
   private val secondNum: Int = readInt()
   private val thirdNum: Int = readInt()
+  //try catch exception handling
   try{
     if(thirdNum == 0){
       throw new ArithmeticException
